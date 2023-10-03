22dfd6bb1e4b542468796b4bc37ee10f0e2142c0
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-10-03 10:08:37 +0530

    sound board project

---
+++ 09-Sound-Board/index.js
@@ -0,0 +1,25 @@
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+
+const buttons = document.getElementById('buttons');
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+        document.getElementById(sound).play()
+    })
+
+    buttons.appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound);
+        song.pause()
+        song.currentTime = 0;
+    })
+}
